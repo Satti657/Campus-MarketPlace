bf6d7627ece957efecbeeb6e6f46b6a1ca09eb39
Satti657 <sattiarslan24@gmail.com>
2026-09-19 00:39:30 +0500

    Complete Day 5 JWT authentication

--- server.js
+++ server.js
@@ -197,8 +197,8 @@ app.post('/signup', async (req, res) => {
 
         const passwordHash = await bcrypt.hash(password, 10);
 
-       const result = await pool.query(
-    `INSERT INTO users (name, email, password)
+    const result = await pool.query(
+    `INSERT INTO users (name, email, password_hash)
      VALUES ($1, $2, $3)
      RETURNING id, name, email`,
     [name, email, passwordHash]
@@ -232,7 +232,7 @@ app.post('/login', async (req, res) => {
 
         // Find user
         const result = await pool.query(
-            'SELECT id, name, email, password FROM users WHERE email = $1',
+            'SELECT id, name, email, password_hash FROM users WHERE email = $1',
             [email]
         );
 
@@ -245,7 +245,7 @@ app.post('/login', async (req, res) => {
         const user = result.rows[0];
 
         // Compare password with bcrypt hash
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcrypt.compare(password, user.password_hash);
 
         if (!isMatch) {
             return res.status(401).json({
